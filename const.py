b13eb1adfd2ea85176912b16025b8d920d3f85f9
Jack Cherng <jfcherng@gmail.com>
2020-10-03 10:03:53 +0800

    chore: update Pylance 2020.9.8
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- const.py
+++ const.py
@@ -2,7 +2,7 @@ import sublime
 
 
 # @see https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance
-SERVER_VERSION = "2020.9.7"
+SERVER_VERSION = "2020.9.8"
 
 PLUGIN_NAME = "LSP-pylance"
 SETTINGS_FILENAME = "{}.sublime-settings".format(PLUGIN_NAME)
