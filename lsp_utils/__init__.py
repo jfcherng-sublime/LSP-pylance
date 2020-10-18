e5de32d0b72e6187daa2c822163fb02d92e82659
Jack Cherng <jfcherng@gmail.com>
2020-10-18 15:20:59 +0800

    refactor: utilize the VSCode marketplace client handler
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

---
+++ lsp_utils/__init__.py
@@ -0,0 +1,16 @@
+try:
+    from LSP.plugin import __version__ as lsp_version
+except ImportError:
+    lsp_version = (0, 0, 0)
+
+from .server_vscode_marketplace_resource import ServerVscodeMarketplaceResource
+
+if lsp_version >= (1, 0, 0):
+    from .vscode_marketplace_client_handler_v2 import VscodeMarketplaceClientHandler
+else:
+    from .vscode_marketplace_client_handler import VscodeMarketplaceClientHandler
+
+__all__ = [
+    'VscodeMarketplaceClientHandler',
+    'ServerVscodeMarketplaceResource',
+]
