df9a0af51b427a6fd8840d1b203aeec1fa832471
Jack Cherng <jfcherng@gmail.com>
2020-10-22 09:28:58 +0800

    chore: update Pylance 2020.10.2
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -8,6 +8,6 @@ class LspPylancePlugin(VscodeMarketplaceClientHandler):
 
     # @see https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance
     extension_item_name = "ms-python.vscode-pylance"
-    extension_version = "2020.10.1"
+    extension_version = "2020.10.2"
     server_binary_path = os.path.join("extension", "dist", "server.bundle.js")
     execute_with_node = True
