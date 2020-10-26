6dc94e218280885b0a171d039b9ff97f90366e00
Jack Cherng <jfcherng@gmail.com>
2020-10-26 23:45:34 +0800

    fix: add missing variables expansion
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -1,4 +1,7 @@
 import os
+import sublime
+
+from LSP.plugin.core.typing import Dict, Optional, Tuple
 
 from .lsp_utils import VscodeMarketplaceClientHandler
 
@@ -11,3 +14,18 @@ class LspPylancePlugin(VscodeMarketplaceClientHandler):
     extension_version = "2020.10.2"
     server_binary_path = os.path.join("extension", "dist", "server.bundle.js")
     execute_with_node = True
+
+    @classmethod
+    def additional_variables(cls) -> Optional[Dict[str, str]]:
+        variables = super().additional_variables() or {}
+        variables.update(
+            {
+                "sublime_py_files_dir": os.path.dirname(sublime.__file__),
+            }
+        )
+
+        return variables
+
+    @classmethod
+    def minimum_node_version(cls) -> Tuple[int, int, int]:
+        return (12, 0, 0)
