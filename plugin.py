0d740791dce6399b85cad30c919aa965c5d8ca38
Jack Cherng <jfcherng@gmail.com>
2020-10-29 19:17:18 +0800

    fix: temp downgrade to 2020.10.2
    Microsoft emits error in non-MS editors as of 2020.10.3
    
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -11,7 +11,7 @@ class LspPylancePlugin(VscodeMarketplaceClientHandler):
 
     # @see https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance
     extension_item_name = "ms-python.vscode-pylance"
-    extension_version = "2020.10.3"
+    extension_version = "2020.10.2"
     server_binary_path = os.path.join("extension", "dist", "server.bundle.js")
     execute_with_node = True
 
