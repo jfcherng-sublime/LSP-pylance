7db3069b77816331a82783e9325cdb06701d18fd
Jack Cherng <jfcherng@gmail.com>
2020-10-30 10:20:39 +0800

    chore: add some comments
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- plugin.py
+++ plugin.py
@@ -2,7 +2,7 @@ import json
 import os
 import sublime
 
-from LSP.plugin.core.typing import Dict, Optional, Tuple
+from LSP.plugin.core.typing import Any, Dict, Optional, Tuple
 
 from .lsp_utils import VscodeMarketplaceClientHandler
 
@@ -17,7 +17,8 @@ class LspPylancePlugin(VscodeMarketplaceClientHandler):
     execute_with_node = True
 
     @classmethod
-    def on_client_configuration_ready(cls, configuration: Dict) -> None:
+    def on_client_configuration_ready(cls, configuration: Dict[str, Any]) -> None:
+        # add necessary env variables in order to use Pylance as of 2020.10.3
         configuration["env"].update(
             {
                 "ELECTRON_RUN_AS_NODE": "1",
@@ -35,6 +36,7 @@ class LspPylancePlugin(VscodeMarketplaceClientHandler):
         variables = super().additional_variables() or {}
         variables.update(
             {
+                # handy for ST plugin dev to include "sublime.py" and "sublime_plugin.py"
                 "sublime_py_files_dir": os.path.dirname(sublime.__file__),
             }
         )
