9eba882435113604c3087f1024903728d0d1e91a
Jack Cherng <jfcherng@gmail.com>
2020-11-20 12:11:12 +0800

    chore: remove useless "type: ignore"
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- helpers/vs_marketplace_lsp_utils/__init__.py
+++ helpers/vs_marketplace_lsp_utils/__init__.py
@@ -3,7 +3,7 @@ from .client_handler_decorator import request_handler
 from LSP.plugin import __version__ as lsp_version
 from lsp_utils.api_wrapper import ApiWrapperInterface
 
-if lsp_version >= (1, 0, 0):  # type: ignore
+if lsp_version >= (1, 0, 0):
     # ST 4
     from .vs_marketplace_client_handler_v2 import VsMarketplaceClientHandler
 else:
