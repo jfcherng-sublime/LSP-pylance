696c52e8c1fcac797b260248013eb428e8cd0586
Jack Cherng <jfcherng@gmail.com>
2020-11-28 14:19:32 +0800

    chore: remove unused import
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- helpers/vs_marketplace_lsp_utils/__init__.py
+++ helpers/vs_marketplace_lsp_utils/__init__.py
@@ -2,11 +2,8 @@ from .server_vs_marketplace_resource import ServerVsMarketplaceResource
 from .vs_marketplace_client_handler import VsMarketplaceClientHandler
 from .vscode_settings import configure_lsp_like_vscode
 from .vscode_settings import configure_server_settings_like_vscode
-from LSP.plugin import __version__ as lsp_version
-from lsp_utils import ApiWrapperInterface
 
 __all__ = [
-    "ApiWrapperInterface",
     "configure_lsp_like_vscode",
     "configure_server_settings_like_vscode",
     "ServerVsMarketplaceResource",
